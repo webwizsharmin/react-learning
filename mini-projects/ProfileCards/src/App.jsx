3d94f86ed01cf97b505d36622f25e1fc1e0b84e0
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2026-07-30 15:17:02 +0600

    feat: add reusable input

--- mini-projects/ProfileCards/src/App.jsx
+++ mini-projects/ProfileCards/src/App.jsx
@@ -1,7 +1,8 @@
 import Button from "./components/Buttons/ButtonR";
+import Input from "./components/UI/inputs";
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 gap-6">
+    <div className="flex flex-col items-center justify-center bg-gray-100 gap-6 p-6">
       <Button variant="primary">Save Client</Button>
       <Button variant="secondary" size="sm">
         Cancel
@@ -13,6 +14,22 @@ function App() {
       <Button variant="warning">Archive</Button>
       <Button variant="outline">Back</Button>
       <Button variant="ghost">View Details</Button>
+      <Input
+        type="email"
+        label="Client Email"
+        placeholder="Enter Email"
+        onChange={() => {}}
+        error=""
+        required
+      />
+      <Input
+        type="password"
+        label="Client password"
+        placeholder="Enter Password"
+        onChange={() => {}}
+        error=""
+        required
+      />
       <Button variant="primary" fullWidth>
         Submit Form
       </Button>
---
+++ mini-projects/ProfileCards/src/components/UI/inputs.jsx
@@ -0,0 +1,37 @@
+const Input = ({
+  type = "text",
+  label,
+  placeholder,
+  value,
+  onChange,
+  error,
+  required = false,
+}) => {
+  const id = label.toLowerCase().replace(/\s+g/, "-");
+
+  return (
+    <div className="mb-4">
+      {label && (
+        <label
+          htmlFor={id}
+          className="block text-sm font-medium text-gray-700 mb-1"
+        >
+          {label} {required && <span className="text-red-500">*</span>}
+        </label>
+      )}
+
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        required={required}
+        className={`w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 ${error ? "border-red-500" : "border-gray-300"} `}
+      />
+      {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
+    </div>
+  );
+};
+
+export default Input;
