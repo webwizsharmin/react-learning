308f7a540af7eb1e5ed9546f3ab2026708267fa1
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2026-08-04 12:35:08 +0600

    fix: update taskLists file name to taskData

---
+++ mini-projects/ListManager/src/components/taskData.jsx
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    title: "learn useState",
+    description: "useState is a hook of react",
+    action: "Edit",
+  },
+  {
+    title: "Build a counter",
+    description: "using useState a counter app need to build ",
+    action: "save",
+  },
+  {
+    title: "event handling",
+    description: "useState is a hook of react",
+    action: "Edit",
+  },
+  {
+    title: "Build list Manager app",
+    description: "useState is a hook of react",
+    action: "Edit",
+  },
+];
