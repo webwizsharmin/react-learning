fe3caf967e8b50b2d8abf96960b421a1899e6911
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2026-08-04 13:02:24 +0600

    fix: update the UI

--- mini-projects/ListManager/src/App.jsx
+++ mini-projects/ListManager/src/App.jsx
@@ -1,43 +1,46 @@
+import { useState } from "react";
 import ListManager from "./components/ListManager";
+import { tasks } from "./components/taskData";
 
 function App() {
-  const tasks = [
-    {
-      title: "learn useState",
-      description: "useState is a hook of react",
-      action: "Edit",
-    },
-    {
-      title: "Build a counter",
-      description: "using useState a counter app need to build ",
-      action: "save",
-    },
-    {
-      title: "learn useState",
-      description: "useState is a hook of react",
-      action: "Edit",
-    },
-    {
-      title: "learn useState",
-      description: "useState is a hook of react",
-      action: "Edit",
-    },
-  ];
+  const [index, setIndex] = useState(0);
+
+  function handleClick() {
+    setIndex(index + 1);
+  }
+
+  function handlePrevClick() {
+    setIndex(index - 1);
+  }
+
+  let task = tasks[index];
 
   return (
     <div className="bg-gray-700  p-6">
       <h2 className="text-5xl font-extrabold text-green-600 mb-4">
         List of Tasks
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
-        {tasks.map((task, index) => (
-          <ListManager
-            key={index}
-            title={task.title}
-            description={task.description}
-            action={task.action}
-          />
-        ))}
+      <div className="flex gap-4 mb-2">
+        <button
+          onClick={handlePrevClick}
+          className="px-4 py-2 border rounded-lg bg-gray-300 hover:bg-gray-400"
+        >
+          prev
+        </button>
+        <button
+          onClick={handleClick}
+          className="px-4 py-2 border rounded-lg bg-gray-300 hover:bg-gray-400"
+        >
+          Next
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-2">
+        <ListManager
+          key={index}
+          title={task.title}
+          description={task.description}
+          action={task.action}
+        />
       </div>
     </div>
   );
--- mini-projects/ListManager/src/components/ListManager.jsx
+++ mini-projects/ListManager/src/components/ListManager.jsx
@@ -1,11 +1,14 @@
-export default function ListManager({ title, description, action }) {
+import Button from "./Button";
+
+export default function ListManager({ title, description }) {
   return (
     <div className="bg-white rounded-lg shadow-md p-6">
       <h1 className="text-3xl font-bold">{title}</h1>
       <p className="text-sm">{description}</p>
-      <button className="px-4 py-2 bg-blue-500 hover:bg-blue-600">
-        {action}
-      </button>
+      <div className="flex gap-2">
+        <Button variant="primary">Edit</Button>
+        <Button variant="danger">Remove</Button>
+      </div>
     </div>
   );
 }
