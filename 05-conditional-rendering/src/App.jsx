fa857cceb2a15b93903934d25c99d5dc3dd38eb5
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2026-08-08 09:52:10 +0600

    add welcomeMessages and render

--- 05-conditional-rendering/src/App.jsx
+++ 05-conditional-rendering/src/App.jsx
@@ -1,3 +1,21 @@
-function App() {}
+import NotificationMessage from "./components/NotificationMessage";
+import UserProfile from "./components/UserProfile";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage isLoggedIn={true} />
+      <NotificationMessage />
+      <UserProfile
+        isLoading={false}
+        data={{
+          name: "Alex",
+          age: 20,
+        }}
+      />
+    </div>
+  );
+}
 
 export default App;
--- 05-conditional-rendering/src/components/WelcomeMessage.jsx
+++ 05-conditional-rendering/src/components/WelcomeMessage.jsx
@@ -1,5 +1,4 @@
 const WelcomeMessage = (isLoggedIn) => {
-  isLoggedIn = false;
   return (
     <div>{isLoggedIn ? <h1>Welcome Back</h1> : <h1>Please Sing In.</h1>}</div>
   );
